fcff3f33f52c2ed57df7939fe26b7304346c509b
Shay Ronen <47533456+wwwshayronen@users.noreply.github.com>
2021-04-14 19:19:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/public/index.html'));
 })
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 function startServer() {
   app.listen(port, () => {
