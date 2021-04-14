7085e7120542ed3bcc4b661a7f4173d33f757804
Shay Ronen <47533456+wwwshayronen@users.noreply.github.com>
2021-04-14 18:53:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -28,6 +28,18 @@ app.post("/api/switch", function (req, res) {
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
+
 const PORT = process.env.PORT || 5000;
 
 function startServer() {
